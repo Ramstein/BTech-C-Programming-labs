23523629ddc67c461b72d7399b5a45daa88c78ca
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-03-17 23:37:53 +0530

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("Good luck against the grovers!");
+}
---
+++ Source3.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i, j;
+	for (i = 0; i <= 4; i++)
+		j = increment(i);
+	printf("%d", j);
+	return 0;
+}
+
+int increment(int i)
+{
+	static int count = 0;
+	count = count + 1;
+	return(count);
+}
