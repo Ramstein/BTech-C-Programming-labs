94a0abe7170ebbbe88a01cbf81e01b29b6a24add
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-03-17 23:39:31 +0530

    Add files via upload

---
+++ ConsoleApplication47.c
@@ -0,0 +1,13 @@
+// ConsoleApplication47.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include<stdio.h>
+
+int main()
+{
+	int num[] = { 1,2,3,4,5,6,7,8,9,20 };
+	for (int x : num) {
+		printf("%d  ", x);
+
+	}
+}
